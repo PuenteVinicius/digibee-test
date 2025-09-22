f90f79b34cf59e7a3cc467d4682af376b093a72f
Vini <vinicius.puente@caju.com.br>
2025-09-22 13:28:58 -0300

    [fix] test config

--- vitest.config.ts
+++ vitest.config.ts
@@ -9,7 +9,6 @@ const dirname =
     ? __dirname
     : path.dirname(fileURLToPath(import.meta.url));
 
-// More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
   plugins: [],
   resolve: {
@@ -33,25 +32,5 @@ export default defineConfig({
         "**/node_modules/**",
       ],
     },
-    projects: [
-      {
-        extends: true,
-        plugins: [],
-        test: {
-          name: "storybook",
-          browser: {
-            enabled: true,
-            headless: true,
-            provider: "playwright",
-            instances: [
-              {
-                browser: "chromium",
-              },
-            ],
-          },
-          setupFiles: [".storybook/vitest.setup.ts"],
-        },
-      },
-    ],
   },
 });
